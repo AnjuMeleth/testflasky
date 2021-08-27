fdfe82123903ad49c418e6672a7947f0eb6824dc
AnjuMeleth <46922680+AnjuMeleth@users.noreply.github.com>
2021-08-27 19:41:37 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -41,7 +41,6 @@ def create_app(config_name):
 
     from .api import api as api_blueprint
     app.register_blueprint(api_blueprint, url_prefix='/api/v1')
-    print("Hello, World!")
-    print( app.config['SECRET_KEY'])
+    
 
     return app
